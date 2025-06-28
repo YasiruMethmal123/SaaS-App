7dc6b5eb71ce747a012776af0d2021a54eb2ac06
YasiruMethmal123 <yasiru.methmal@outlook.com>
2025-06-28 23:51:15 +0530

    companionCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,41 @@
+import CompanionCard from '@/components/CompanionCard'
 import Navbar from '@/components/Navbar'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <Navbar/>
+    <main>
+      <h1>Profiel Section</h1>
+      <section className="home-section">
+        <CompanionCard
+            id = "123"
+            name = "Artifitial Inteligence"
+            topic="Nural Networking"
+            subject = "AI"
+            duration = {45}
+            color="#687FE5"
+          />
+
+          <CompanionCard
+            id = "234"
+            name = "Calculus"
+            topic="Differentiation"
+            subject = "Maths"
+            duration = {45}
+            color="#FCD8CD"
+          />
+
+          <CompanionCard
+            id = "235"
+            name = "Thermal Physics"
+            topic="Standard measuring"
+            subject = "Physics"
+            duration = {45}
+            color="#EBD6FB"
+          />
+      </section>
+    </main>
   )
 }
 
---
+++ lib/action/companion.action.ts
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const companion.action = () => {
+  return (
+    <div>companion.action</div>
+  )
+}
+
+export default companion.action
